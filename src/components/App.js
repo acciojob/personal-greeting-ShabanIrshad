f28626064325f9c2d31c6b77d759a4ee77dd43ab
Shaban Irshad <shabanirshad408@gmail.com>
2026-03-05 14:49:37 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState, useTransition } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName]=useState('');
   return (
     <div>
-        {/* Do not remove the main div */}
+       <form>
+          <label htmlFor="name">Enter your name:</label>
+          <input type="text" id="name" placeholder="Enter Name..." onChange={(e)=>setName(e.target.value)} />
+          {name?<p>Hello, {name}! </p>:''}
+       </form>
     </div>
   )
 }
